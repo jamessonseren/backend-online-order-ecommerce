b4334730abe008d40c8971dc81e29b8ad2f964d1
Jamesson Seren de Arruda <jamesson.arruda@sga.pucminas.br>
2023-06-28 21:25:30 -0400

    updated vercel.json

---
+++ public/prisma/seeds.js
@@ -0,0 +1,108 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+const snacks = [
+    {
+        snack: "burger",
+        name: "Mega",
+        description: "Tamanho artesanal família recheado com três carnes suculentas, queijo e bacon.",
+        price: 7.75,
+        image: "https://i.imgur.com/L5IhOun.png"
+    },
+    {
+        snack: "burger",
+        name: "Big Burger Bacon",
+        description: "Criado para os amantes e bacon, possui em todas as camadas bacon bem assado.",
+        price: 8.25,
+        image: "https://i.imgur.com/3ccsTEQ.jpeg"
+    },
+    {
+        snack: "dessert",
+        name: "Cookie Dough",
+        description: "It makes for a fun appetizer or dessert for any party, bridal or baby shower, or a yummy snack to share!  Give it a try!  It is AMAZING!",
+        price: 9.75,
+        image: "https://i.imgur.com/bFH4vVe.jpeg"
+    },
+    {
+        snack: "dessert",
+        name: "Snickers Dip",
+        description: "This Snickers Dip is the perfect sweet appetizer to feed a crowd! And well, it’s an easy sweet dip made of Snickers. How can that not be amazing?",
+        price: 6.85,
+        image: "https://i.imgur.com/5sjJrIb.jpeg"
+    },
+    {
+        snack: "dessert",
+        name: "Lemon Cream Cheese",
+        description: "The easiest cream cheese fruit dip, this Lemon Cream Cheese Fruit Dip has marshmallow creme and cream cheese, plus lemon yogurt to give it the best lemon flavor.",
+        price: 7.10,
+        image: "https://i.imgur.com/yaARGHk.jpeg"
+    },
+    {
+        snack: "drink",
+        name: "Vanilla Vodka",
+        description: "Delicious drink with Whiskey, Apple Juice, and Pinch of Ground Cinnamon",
+        price: 8.5,
+        image: "https://i.imgur.com/fIf86bh.jpeg"
+    },
+    {
+        snack: "drink",
+        name: "Amazing Apple Juice",
+        description: "Apple juice with ginger beer, seltzer, and bourbon",
+        price: 6.85,
+        image: "https://i.imgur.com/NOZtMV3.png"
+    },
+    {
+        snack: "drink",
+        name: "Special Orang Juice",
+        description: "Orange juice with maple syrup and angostura bitters.",
+        price: 7.10,
+        image: "https://i.imgur.com/j1zL2pB.jpeg"
+    },
+    {
+        snack: "pizza",
+        name: "Pepperoni",
+        description: "Brazilian style food with pepperoni, cheese, and onions",
+        price: 9.56,
+        image: "https://media.istockphoto.com/id/1314368528/pt/foto/brazilian-style-pizza-with-mozzarella-cheese-pepperoni-sausage-and-onion-top-view.webp?s=1024x1024&w=is&k=20&c=D99g39zgfKcblIVeCA_n5YjKRR6luulHpgZXE0M0Bxw="
+    },
+    {
+        snack: "pizza",
+        name: "Margherita",
+        description: "Delicious italian Margherita pizza with tomatoes and cheese",
+        price: 9.85,
+        image: "https://media.istockphoto.com/id/1414575281/pt/foto/a-delicious-and-tasty-italian-pizza-margherita-with-tomatoes-and-buffalo-mozzarella.webp?s=1024x1024&w=is&k=20&c=0z2tURHptlwSCz_r33U8y_1z7K5cge-3wX2BACdOi_Y="
+    },
+    {
+        snack: "pizza",
+        name: "Bacon and Brocolli",
+        description: "Delicious Bacon and Brocolli Pizza",
+        price: 8.15,
+        image: "https://media.istockphoto.com/id/1390950332/pt/foto/bacon-and-broccoli-pizza.webp?s=1024x1024&w=is&k=20&c=jb0sOS2TLx3gMVpGpum5kkrTxRJh8CTvLOjgmpuyAeA="
+    }
+];
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield prisma.snack.createMany({
+            data: snacks,
+            skipDuplicates: true
+        });
+    });
+}
+main()
+    .catch((error) => {
+    console.error(error);
+    process.exit(1);
+})
+    .finally(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield prisma.$disconnect();
+}));
---
+++ public/src/index.js
@@ -0,0 +1,57 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const CheckoutService_1 = __importDefault(require("./services/CheckoutService"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const prisma = new client_1.PrismaClient();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 5000;
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.get("/snacks", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { snack } = req.query;
+    if (!snack)
+        return res.status(400).send({ error: "Snack is required" });
+    const snacks = yield prisma.snack.findMany({
+        where: {
+            snack: snack
+        }
+    });
+    res.send(snacks);
+}));
+app.get("/orders/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const order = yield prisma.order.findUnique({
+        where: {
+            id: parseInt(id)
+        },
+        include: { customer: true, orderItems: { include: { snack: true } } }
+    });
+    if (!order)
+        return res.status(404).send({ error: "Order not found" });
+    res.send(order);
+}));
+app.post('/checkout', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { cart, customer, payment } = req.body;
+    const checkoutService = new CheckoutService_1.default();
+    const orderCreated = yield checkoutService.process(cart, customer, payment);
+    res.send(orderCreated);
+}));
+app.listen(port, () => {
+    console.log(`Server running on port ${port} -  localhost:${port}`);
+});
