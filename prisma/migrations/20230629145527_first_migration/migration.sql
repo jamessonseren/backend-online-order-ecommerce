e7574e9419d32ed9a25b1716ec3360b9756ea34e
Jamesson Seren de Arruda <jamesson.arruda@sga.pucminas.br>
2023-06-29 11:03:40 -0400

    updated schema

--- prisma/migrations/20230629145527_first_migration/migration.sql
+++ prisma/migrations/20230629145527_first_migration/migration.sql
@@ -2,6 +2,20 @@
 CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'PAID', 'CANCELED');
 
 -- CreateTable
+CREATE TABLE "Snack" (
+    "id" SERIAL NOT NULL,
+    "snack" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Snack_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Customer" (
     "id" SERIAL NOT NULL,
     "fullName" VARCHAR(255) NOT NULL,
